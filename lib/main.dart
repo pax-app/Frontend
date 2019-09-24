f74f8ec6494d9b5209cf818568ccf45896acd7e8
Esio Freitas <esio.gustavo@gmail.com>
2019-09-24 15:24:30 -0300

    Add project theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:Pax/theme/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:Pax/screens/home_screen/home_screen.dart';
+
 
 void main() => runApp(PaxApp());
 
@@ -8,13 +10,7 @@ class PaxApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Pax',
-      theme: ThemeData(
-        primaryColor: Color.fromRGBO(124, 179, 66, 1),
-        scaffoldBackgroundColor: Color.fromRGBO(247, 247, 247, 1),
-        textSelectionColor: Color.fromRGBO(28, 25, 25, 1),
-        errorColor: Color.fromRGBO(255, 75, 75, 1),
-
-      ),
+      theme: paxTheme,
       showSemanticsDebugger: false,
       home: HomeScreen(),
     );
---
+++ lib/theme/theme.dart
@@ -0,0 +1,22 @@
+import 'package:Pax/theme/colors.dart' as prefix0;
+import 'package:flutter/material.dart';
+
+import 'colors.dart';
+
+final ThemeData paxTheme = _buildPaxTheme();
+
+ThemeData _buildPaxTheme() {
+  final ThemeData base = ThemeData.light();
+  return base.copyWith(
+    accentColor: secondaryColor,
+    primaryColor: primaryColor,
+    buttonTheme: base.buttonTheme.copyWith(
+      buttonColor: secondaryColor,
+      textTheme: ButtonTextTheme.normal,
+    ),
+    scaffoldBackgroundColor: backgroundColor,
+    cardColor: backgroundColor,
+    textSelectionColor: secondaryColor,
+    errorColor: errorColor,
+  );
+}
