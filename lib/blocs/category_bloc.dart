d155b7d7285bb43afa5673df5ac4e38c04d62a51
Esio Freitas <esio.gustavo@gmail.com>
2019-09-29 16:28:56 -0300

    "#141" Add category bloc
    Co-authored-by: FabianaRibas <fabianalv.ribas@gmail.com>
    Signed-off-by: Esio Freitas <esio.gustavo@gmail.com>

---
+++ lib/blocs/category_bloc.dart
@@ -0,0 +1,37 @@
+import 'dart:async';
+import 'package:Pax/models/category.dart';
+import 'package:bloc_pattern/bloc_pattern.dart';
+
+class CategoryBloc implements BlocBase {
+  List<Category> categories;
+  List<Category> apiCategories;
+
+  final _searchController = StreamController<String>();
+  Sink get inSearch => _searchController.sink;
+
+  final _categoryController = StreamController<List<Category>>();
+  Stream get outCategories => _categoryController.stream;
+
+  CategoryBloc() {
+    _searchController.stream.listen(_search);
+    apiCategories.add(Category(id: 1, name: "Aparelho de Som"));
+    apiCategories.add(Category(id: 2, name: "Cabeamento e Redes"));
+    apiCategories.add(Category(id: 3, name: "Celular"));
+  }
+
+  void _search(String search) async {
+    if (search != null) {
+      categories =
+          apiCategories.where((category) => category.name.startsWith(search));
+    } else {
+      categories = List<Category>();
+    }
+    _categoryController.sink.add(categories);
+  }
+
+  @override
+  void dispose() {
+    _categoryController.close();
+    _searchController.close();
+  }
+}
