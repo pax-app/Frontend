7fc923f1dfe6352433d3dc47ab723adea1ab12af
Felipe Campos <fepas.unb@gmail.com>
2019-10-01 22:58:48 -0300

    "pax-app/Wiki#142" adding image input button
    Signed-off-by: Felipe Campos <fepas.unb@gmail.com>
    Co-authored-by: EsioFreitas <esio.gustavo@gmail.com>

--- lib/components/text_input/text_input.dart
+++ lib/components/text_input/text_input.dart
@@ -4,43 +4,49 @@ class TextInput extends StatelessWidget {
   final String textLabel, placeholder;
   final bool small;
   final Function validator;
+  final TextInputType inputType;
 
-  TextInput(this.textLabel, this.placeholder, this.small, this.validator);
+  TextInput(this.textLabel, this.placeholder, this.small, this.validator,
+      this.inputType);
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      decoration: InputDecoration(
-        hintText: textLabel,
-        labelText: placeholder,
-        labelStyle: TextStyle(color: Theme.of(context).accentColor),
-        enabledBorder: OutlineInputBorder(
-          borderSide: BorderSide(
-              color: Theme.of(context).primaryColor,
-              style: BorderStyle.solid,
-              width: 1.0),
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 5.0),
+      child: TextFormField(
+        decoration: InputDecoration(
+          hintText: placeholder,
+          labelText: textLabel,
+          labelStyle: TextStyle(color: Theme.of(context).primaryColor),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+                color: Theme.of(context).primaryColor,
+                style: BorderStyle.solid,
+                width: 1.0),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+                color: Theme.of(context).accentColor,
+                style: BorderStyle.solid,
+                width: 1.0),
+          ),
+          errorBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+                color: Theme.of(context).errorColor,
+                style: BorderStyle.solid,
+                width: 1.0),
+          ),
+          focusedErrorBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+                color: Theme.of(context).errorColor,
+                style: BorderStyle.solid,
+                width: 1.0),
+          ),
         ),
-        focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(
-              color: Theme.of(context).accentColor,
-              style: BorderStyle.solid,
-              width: 1.0),
-        ),
-        errorBorder: OutlineInputBorder(
-          borderSide: BorderSide(
-              color: Theme.of(context).errorColor,
-              style: BorderStyle.solid,
-              width: 1.0),
-        ),
-        focusedErrorBorder: OutlineInputBorder(
-          borderSide: BorderSide(
-              color: Theme.of(context).errorColor,
-              style: BorderStyle.solid,
-              width: 1.0),
-        ),
+        validator: validator,
+        keyboardType: inputType,
+        style: TextStyle(color: Theme.of(context).primaryColor),
       ),
-      validator: validator,
-      style: TextStyle(color: Theme.of(context).primaryColor),
     );
   }
 }
--- lib/screens/became_provider_screen/became_provider_tabs/sign_up_tab.dart
+++ lib/screens/became_provider_screen/became_provider_tabs/sign_up_tab.dart
@@ -14,26 +14,53 @@ class _SignUpTabState extends State<SignUpTab> {
       children: <Widget>[
         TextInput('Nome', 'Nome Completo', true, (String value) {
           return value.contains('@') ? 'Do not use the @ char.' : null;
-        }),
-        SizedBox(
-          height: 20.0,
+        }, TextInputType.text),
+        TextInput('CPF', '000.000.000-00', true, (String value) {
+          return value.contains('@') ? 'Do not use the @ char.' : null;
+        }, TextInputType.number),
+        TextInput('RG', 'RG', true, (String value) {
+          return value.contains('@') ? 'Do not use the @ char.' : null;
+        }, TextInputType.number),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 5.0, vertical: 10.0),
+          child: InkWell(
+            onTap: () {},
+            child: new Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Theme.of(context).primaryColor),
+                      borderRadius: BorderRadius.circular(3.0)),
+                  child: Row(
+                    children: <Widget>[
+                      new Icon(
+                        Icons.cloud_upload,
+                        color: Theme.of(context).primaryColorLight,
+                      ),
+                      SizedBox(width: 8.0),
+                      new Text('SELFIE COM O RG'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
-        Row(
-          children: <Widget>[
-            TextInput('Nome', 'Nome Completo', true, (String value) {
-              return value.contains('@') ? 'Do not use the @ char.' : null;
-            })
-          ],
-        ),
         SizedBox(
-          height: 20.0,
+          height: 50.0,
         ),
         Button(
           buttonText: 'Próximo',
           type: 'default',
           tapHandler: () {},
           isSmall: false,
-        )
+        ),
+        SizedBox(
+          height: 50.0,
+        ),
       ],
     );
   }
