deed15473ce3dd32299333df2489d35919ae8e0d
Rogério Júnior <junior.rogerio8@gmail.com>
2019-10-06 17:38:58 -0300

    pax-app/Wiki#160 Initial message card structure
    Co-authored-by: Youssef Muhamad <emaildeyoussefmuhamad@gmail.com>

---
+++ lib/components/message/message.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Message extends StatelessWidget {
+  final String message;
+  final String hour;
+
+  Message({this.message, this.hour});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Card(),
+    );
+  }
+}
