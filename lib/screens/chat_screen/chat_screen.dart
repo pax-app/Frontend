b5ab64c68f43909b4209bad423fe15817f805ff6
youssef-md <emaildeyoussefmuhamad@gmail.com>
2019-10-10 02:01:08 -0300

    pax-app/Wiki#160 Turn chat screen in a statefull widget and add a simple form for testing

--- lib/screens/chat_screen/chat_screen.dart
+++ lib/screens/chat_screen/chat_screen.dart
@@ -1,8 +1,15 @@
-import 'package:Pax/components/app_bar/white_appbar.dart';
 import 'package:Pax/components/chat_app_bar/chat_app_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _messageController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,12 +25,26 @@ class ChatScreen extends StatelessWidget {
           provider_qualification: "Assistência Técnica: Notebook",
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.send),
+        onPressed: () {},
+      ),
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
             image: AssetImage("assets/illustrations/circle-pattern.png"),
             fit: BoxFit.cover,
           ),
+        ),
+        child: Column(
+          children: <Widget>[
+            Form(
+              child: TextFormField(
+                controller: _messageController,
+                decoration: InputDecoration(labelText: "Send a message"),
+              ),
+            ),
+          ],
         ),
       ),
     );
