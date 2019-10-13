5cf5f18d9d8d41b4db3c1a250fc68ecaf530317f
Esio Freitas <esio.gustavo@gmail.com>
2019-10-13 13:12:16 -0300

    pax-app/Wiki#142 add category api

---
+++ lib/services/category-api.dart
@@ -0,0 +1,9 @@
+import 'package:http/http.dart' as http;
+
+class CategoryApi {
+  final String url;
+
+  CategoryApi(this.url);
+
+  getAllCategories() async {}
+}
--- lib/services/main-api.dart
+++ lib/services/main-api.dart
@@ -1,8 +1,9 @@
+import 'package:Pax/services/category-api.dart';
 import 'package:Pax/services/provider-api.dart';
 import 'package:Pax/services/user-api.dart';
 
 class MainApi {
-  
   final ProviderApi providerApi = ProviderApi("");
   final UserApi userApi = UserApi("");
+  final CategoryApi categoryApi = CategoryApi("");
 }
