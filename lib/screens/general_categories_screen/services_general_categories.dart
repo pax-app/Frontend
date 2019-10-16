69d4a57d437fd56cee04bdd5baa9cf4a18fdd609
Felipe Campos <fepas.unb@gmail.com>
2019-10-15 23:46:38 -0300

    pax-app/Wiki#162 first fetch from api in general category screen
    Co-authored-by: Fabiana Ribas <fabianalv.ribas@gmail.com>

--- lib/screens/general_categories_screen/services_general_categories.dart
+++ lib/screens/general_categories_screen/services_general_categories.dart
@@ -1,64 +1,158 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:Pax/components/general_categories_panel/general_categories_panel_card.dart';
 import 'package:flutter/material.dart';
-import 'package:Pax/models/GeneralCategory.dart';
-import 'package:Pax/blocs/general_category_bloc.dart';
-import 'package:bloc_pattern/bloc_pattern.dart';
-import 'package:Pax/components/provider_panel_card/provider_panel_card.dart';
-import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
-import 'Webservice.dart';
 
-class ServiceGeneralCategory extends StatefulWidget {
-  ServiceGeneralCategory({Key key}) : super(key: key);
-  _ServiceGeneralCategoryState createState() => _ServiceGeneralCategoryState();
+Future<List<Category>> fetchPost() async {
+  final response = await http.get('http://10.0.2.2:5002/category/general',
+      headers: {HttpHeaders.contentTypeHeader: 'application/json'});
+  var responseJson = json.decode(response.body);
+  responseJson = responseJson["data"];
+  responseJson = responseJson["categories"];
+
+  final List<Category> listaFinal = [];
+
+  for (var item in responseJson) {
+    listaFinal.add(Category.fromJson(item));
+  }
+  return listaFinal;
 }
 
-class _ServiceGeneralCategoryState extends State<ServiceGeneralCategory> {
-  List<GeneralCategory> _generalCategories = <GeneralCategory>[];
+class Category {
+  final int id;
+  final String name;
 
-  @override
-  void initState() {
-    super.initState();
-    listenForGeneralCategories();
-  }
+  Category({this.id, this.name});
 
-  void listenForGeneralCategories() async {
-    final Stream<GeneralCategory> stream = await getGeneralCategories();
-    stream.listen((GeneralCategory generalCategory) =>
-        setState(() => _generalCategories.add(generalCategory)));
+  factory Category.fromJson(Map<String, dynamic> json) {
+    return Category(
+      id: json['general_category.id'],
+      name: json['general_category.name'],
+    );
   }
+}
+
+class ServiceGeneralCategory extends StatelessWidget {
+  final Future<Category> category;
+
+  ServiceGeneralCategory({Key key, this.category}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     const Map<String, String> imagesPaths = {
-      'Domesticos': 'assets/categories-img/clean.png',
-      'Tecnologia': 'assets/categories-img/code.png',
-      'Assistencia': 'assets/categories-img/gear.png',
+      'Serviços Domésticos': 'assets/categories-img/clean.png',
+      'Design e Tecnologia': 'assets/categories-img/code.png',
+      'Assistência Técnica': 'assets/categories-img/gear.png',
       'Reformas': 'assets/categories-img/paint.png',
     };
-    return SingleChildScrollView(
-      child: Column(
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.only(bottom: 30, right: 10, left: 10, top: 20),
-            child: ListView(
-              physics: NeverScrollableScrollPhysics(),
-              scrollDirection: Axis.vertical,
-              shrinkWrap: true,
-              children: <Widget>[
-                Text('Hello fabi'),
-                ListView.builder(
-                  itemCount: _generalCategories.length,
-                  itemBuilder: (context, index) => GeneralCategoriesPanelCard(
-                      _generalCategories[index].name,
-                      'bla',
-                      imagesPaths['Domesticos']),
-                ),
-              ],
-            ),
-          )
-        ],
-      ),
+    return FutureBuilder<List<Category>>(
+      future: fetchPost(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          List<Category> categories = snapshot.data;
+          return Column(
+            children: categories
+                .map(
+                  (item) => Container(
+                    child: GeneralCategoriesPanelCard(item.name,
+                        item.id.toString(), imagesPaths['${item.name}']),
+                  ),
+                )
+                .toList(),
+          );
+        } else if (snapshot.hasError) {
+          return Text("${snapshot.error}");
+        }
+
+        // By default, show a loading spinner.
+        return CircularProgressIndicator();
+      },
     );
   }
 }
+
+// import 'dart:convert';
+// import 'package:Pax/components/general_categories_panel/general_categories_panel_card.dart';
+// import 'package:flutter/material.dart';
+// import 'package:Pax/models/GeneralCategory.dart';
+// import 'package:flutter/widgets.dart';
+// import 'package:http/http.dart' as http;
+
+// class ServiceGeneralCategory extends StatefulWidget {
+//   ServiceGeneralCategory({Key key}) : super(key: key);
+//   _ServiceGeneralCategoryState createState() => _ServiceGeneralCategoryState();
+// }
+
+// class _ServiceGeneralCategoryState extends State<ServiceGeneralCategory> {
+//   _fetchData() async {
+//     final response = await http.get("http://10.0.2.2:5002/category/general");
+//     if (response.statusCode == 200) {
+//       return GeneralCategory.fromJson(json.decode(response.body));
+//     } else {
+//       throw Exception('Failed to load general category');
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return FutureBuilder<GeneralCategory>(
+//       future: _fetchData(),
+//       builder: (context, snapshot) {
+//         print(snapshot);
+//         if (snapshot.hasData) {
+//           return Text(snapshot.data.name);
+//         } else if (snapshot.hasError) {
+//           return Text("${snapshot.error}");
+//         }
+
+//         // By default, show a loading spinner.
+//         return CircularProgressIndicator();
+//       },
+//     );
+//   }
+
+//   // @override
+//   // Widget build(BuildContext context) {
+//   //   const Map<String, String> imagesPaths = {
+//   //     'Domesticos': 'assets/categories-img/clean.png',
+//   //     'Tecnologia': 'assets/categories-img/code.png',
+//   //     'Assistencia': 'assets/categories-img/gear.png',
+//   //     'Reformas': 'assets/categories-img/paint.png',
+//   //   };
+//   //   return SingleChildScrollView(
+//   //     child: Column(
+//   //       children: <Widget>[
+//   //         Padding(
+//   //           padding: EdgeInsets.only(bottom: 30, right: 10, left: 10, top: 20),
+//   //           child: ListView(
+//   //             physics: NeverScrollableScrollPhysics(),
+//   //             scrollDirection: Axis.vertical,
+//   //             shrinkWrap: true,
+//   //             children: <Widget>[
+//   //               GeneralCategoriesPanelCard(
+//   //                   'Asistência Técnica',
+//   //                   'Encontre a pessoa certa para consertar seus aparelhos eletrônicos',
+//   //                   imagesPaths['Assistencia']),
+//   //               GeneralCategoriesPanelCard(
+//   //                   'Reformas',
+//   //                   'Encontre a pessoa certa para consertar seus aparelhos eletrônicos',
+//   //                   imagesPaths['Reformas']),
+//   //               GeneralCategoriesPanelCard(
+//   //                   'Serviços Domésticos',
+//   //                   'Encontre a pessoa certa para consertar seus aparelhos eletrônicos',
+//   //                   imagesPaths['Domesticos']),
+//   //               GeneralCategoriesPanelCard(
+//   //                   'Design e Tecnologia',
+//   //                   'Encontre a pessoa certa para consertar seus aparelhos eletrônicos',
+//   //                   imagesPaths['Tecnologia']),
+//   //             ],
+//   //           ),
+//   //         )
+//   //       ],
+//   //     ),
+//   //   );
+//   // }
+// }
