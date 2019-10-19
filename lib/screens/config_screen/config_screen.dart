0ad18da8ff7106ff22c541e587f0598f1d2d58fa
Esio Freitas <esio.gustavo@gmail.com>
2019-10-19 17:00:20 -0300

    pax-app/Wiki#142 Add config screen

--- lib/screens/config_screen/config_screen.dart
+++ lib/screens/config_screen/config_screen.dart
@@ -1,4 +1,6 @@
+import 'package:Pax/components/base_screen/base_screen.dart';
 import 'package:Pax/components/simple_tile/simple_tile.dart';
+import 'package:Pax/screens/config_screen/tabs/edit-profile.dart';
 import 'package:flutter/material.dart';
 
 class ConfigScreen extends StatelessWidget {
@@ -6,7 +8,18 @@ class ConfigScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        SimpleTile("Editar Perfil", (){}),
+        SimpleTile("Editar Perfil", (){
+          Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => BaseScreen(
+                        "",
+                        "",
+                        EditPerfilTab(),
+                        null,
+                      ),
+                    ),
+                  );
+        }),
         SimpleTile("Mudar Senha", (){}),
       ],
     );
