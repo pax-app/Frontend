a732fb6ae31d248f3fe30570698f86a57c175cdd
Rogério Júnior <junior.rogerio8@gmail.com>
2019-10-20 21:51:31 -0300

    pax-app/Wiki#160 [FINALLY] Gets all chats of a user
    Co-authored-by: Youssef Muhamad <emaildeyoussefmuhamad@gmail.com>
    Co-authored-by: Marcos Nery <marcosnery.comp@gmail.com>

--- lib/screens/my_conversations/my_conversations.dart
+++ lib/screens/my_conversations/my_conversations.dart
@@ -1,9 +1,26 @@
 import 'package:Pax/components/chat_tile/chat_tile.dart';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'dart:convert';
 
 class MyConversations extends StatelessWidget {
+  _getUserChats() async {
+    var params = {"user_id": "1"};
+
+    Uri uri = Uri.parse("https://pax-chat.herokuapp.com/chats");
+    final newURI = uri.replace(queryParameters: params);
+
+    var response = await http.get(newURI);
+
+    print('---- status code: ${response.statusCode}');
+    var jsonData = json.decode(response.body);
+
+    print('---- slot: ${jsonData}');
+  }
+
   @override
   Widget build(BuildContext context) {
+    _getUserChats();
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: <Widget>[
