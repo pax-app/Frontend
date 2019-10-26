e2fe870ce2ff20b8c9586c6dc87e1023906cffca
youssef-md <emaildeyoussefmuhamad@gmail.com>
2019-10-26 20:56:29 -0300

    pax-app/Wiki#183 Export provider sheet  as a statefull to rebuild when slider updates

--- lib/models/Provider.dart
+++ lib/models/Provider.dart
@@ -1,10 +1,10 @@
 import 'package:Pax/models/category.dart';
 import 'package:Pax/models/user.dart';
 
-class Provider extends User{
+class Provider extends User {
   String bio = "";
   double minPrice = 0, maxPrice = 0;
-  List<Category> categories =  List<Category>();
+  List<Category> categories = List<Category>();
 
   Provider({this.bio, this.minPrice, this.maxPrice, this.categories});
 
@@ -13,7 +13,7 @@ class Provider extends User{
       bio: json['bio'],
       maxPrice: json['maxPrice'],
       minPrice: json['minPrice'],
-      categories: json['categories'].map((c)=>Category.fromJson(c)).toList(),
+      categories: json['categories'].map((c) => Category.fromJson(c)).toList(),
     );
   }
 }
