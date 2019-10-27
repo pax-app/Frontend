3e4674e4d3c6abd684ca8ca0306e3a8c12bf7251
youssef-md <emaildeyoussefmuhamad@gmail.com>
2019-10-27 18:49:15 -0300

    pax-app/Wiki#184 List view to show all users addresses with a dynamic height

--- lib/screens/chat_screen/chat_address_bottom_sheet.dart
+++ lib/screens/chat_screen/chat_address_bottom_sheet.dart
@@ -4,7 +4,11 @@ import 'package:flutter/material.dart';
 class ChatAddressBottomSheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    int _addressLength = 2;
+    double _currentSheetHeight = 80 + 100 * _addressLength.toDouble();
+
     return BaseBottomSheet(
+      modalHeight: _currentSheetHeight > 380 ? 380 : _currentSheetHeight,
       sheetBody: Column(
         children: <Widget>[
           Padding(
@@ -17,23 +21,30 @@ class ChatAddressBottomSheet extends StatelessWidget {
               textAlign: TextAlign.center,
             ),
           ),
-          SizedBox(height: 38),
-          Column(
-            children: <Widget>[
-              Divider(height: 0, thickness: 1.2),
-              Material(
-                child: InkWell(
-                  onTap: () => {print('foi')},
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 18),
-                    child: Text(
-                      '72871-066 Rua 66 Número 16 Jardim Céu Azul Quadra 88',
-                      textAlign: TextAlign.center,
+          SizedBox(height: 32),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _addressLength,
+              itemBuilder: (context, index) {
+                return Column(
+                  children: <Widget>[
+                    Divider(height: 0, thickness: 1.2),
+                    Material(
+                      child: InkWell(
+                        onTap: () => {print('foi')},
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 20),
+                          child: Text(
+                            '72871-066 Rua 66 Número 16 Jardim Céu Azul Quadra 88',
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-              ),
-            ],
+                  ],
+                );
+              },
+            ),
           ),
         ],
       ),
