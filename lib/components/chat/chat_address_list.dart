d1ce67567c374603397905b786fece97e84d535d
youssef-md <emaildeyoussefmuhamad@gmail.com>
2019-10-29 00:24:40 -0300

    pax-app/Wiki#184 Add address creation button at the end of the address list

--- lib/components/chat/chat_address_list.dart
+++ lib/components/chat/chat_address_list.dart
@@ -11,21 +11,27 @@ class ChatAddressList extends StatelessWidget {
     return ListView.builder(
       itemCount: addressLength,
       itemBuilder: (context, index) {
+        bool isLast = index + 1 == addressLength;
+
         return Column(
           children: <Widget>[
             Divider(height: 1, thickness: 1.2),
             Material(
               child: InkWell(
                 onTap: () => {print('foi')},
-                child: Padding(
+                child: Container(
+                  width: 400,
                   padding: const EdgeInsets.symmetric(
-                    vertical: 15,
+                    vertical: 16,
                     horizontal: 20,
                   ),
                   child: Text(
-                    '${addresses[index]['street']} Número ${addresses[index]['number']}, ${addresses[index]['neighborhood']} - CEP: ${addresses[index]['cep']}',
+                    isLast
+                        ? 'ADICIONAR ENDEREÇO'
+                        : '${addresses[index]['street']} Número ${addresses[index]['number']}, ${addresses[index]['neighborhood']} - CEP: ${addresses[index]['cep']}',
                     textAlign: TextAlign.center,
                     style: TextStyle(
+                      color: isLast ? Theme.of(context).accentColor : null,
                       height: 1.5,
                     ),
                   ),
