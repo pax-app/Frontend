3edbae8b7149eef77e187a45c5c1c6bdf35084f0
youssef-md <emaildeyoussefmuhamad@gmail.com>
2019-11-08 15:11:16 -0300

    pax-app/Wiki#190 Add Base dialog

---
+++ lib/components/base_dialog/base_dialog.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class BaseDialog extends StatelessWidget {
+  final Widget body;
+  final double height;
+
+  const BaseDialog({
+    @required this.body,
+    @required this.height,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Container(
+        height: height,
+        decoration: BoxDecoration(
+          color: Colors.red,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: body,
+      ),
+    );
+  }
+}
