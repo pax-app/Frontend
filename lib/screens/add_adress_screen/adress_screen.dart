8ee163df29953de8ca91dcfceb4ed627fabdf819
youssef-md <emaildeyoussefmuhamad@gmail.com>
2019-11-14 00:07:03 -0300

    pax-app/Wiki#186 Change Text input to use DisabledOutlineInput to match the prototype
    Co-authored-by: Rogério Júnior <junior.rogerio8@gmail.com>

--- lib/screens/add_adress_screen/adress_screen.dart
+++ lib/screens/add_adress_screen/adress_screen.dart
@@ -1,11 +1,10 @@
 import 'package:Pax/components/app_bar/white_appbar.dart';
 import 'package:Pax/components/button%20/button.dart';
+import 'package:Pax/components/disabled_outline_input/disabled_outline_input.dart';
 import 'package:Pax/components/text_input/text_input.dart';
+import 'package:Pax/components/try_again_error/try_again_error.dart';
 import 'package:flutter/material.dart';
 import 'package:via_cep/via_cep.dart';
-import 'package:Pax/models/Address.dart';
-
-import '../../models/Address.dart';
 
 class AdressScreen extends StatefulWidget {
   final String userCep;
@@ -28,166 +27,123 @@ class _AdressScreenState extends State<AdressScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: WhiteAppBar('Adicionar Endereço', context),
-      body: FutureBuilder(
-        future: _getCepData(widget.userCep),
-        builder: (context, snapshot) {
-          if (snapshot.data == null) {
-            return Container(
-              height: MediaQuery.of(context).size.height - 250,
-              child: Center(
-                child: CircularProgressIndicator(),
-              ),
-            );
-          } else if (snapshot.hasData) {
-            _updateTextInputs(snapshot.data);
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                TextInput(
-                  'Rua',
-                  'Insira sua rua',
-                  true,
-                  (String value) {
-                    return value.contains('@')
-                        ? 'Do not use the @ char.'
-                        : null;
-                  },
-                  TextInputType.text,
-                  1,
-                  focus: true,
-                  controller: _street,
-                  enabled: false,
-                ),
-                TextInput(
-                  'Bairro',
-                  'Insira seu bairro',
-                  true,
-                  (String value) {
-                    return value.contains('@')
-                        ? 'Do not use the @ char.'
-                        : null;
-                  },
-                  TextInputType.text,
-                  1,
-                  focus: true,
-                  controller: _neighborhood,
-                  enabled: false,
-                ),
-                TextInput(
-                  'Cidade',
-                  'Insira sua cidade',
-                  true,
-                  (String value) {
-                    return value.contains('@')
-                        ? 'Do not use the @ char.'
-                        : null;
-                  },
-                  TextInputType.text,
-                  1,
-                  focus: true,
-                  controller: _city,
-                  enabled: false,
-                ),
-                Container(
-                  child: Row(
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height - 90,
+          child: FutureBuilder(
+            future: _getCepData(widget.userCep),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                _updateTextInputs(snapshot.data);
+                return Padding(
+                  padding: const EdgeInsets.only(top: 22, left: 30, right: 30),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
                     children: <Widget>[
-                      SizedBox(
-                        width: MediaQuery.of(context).size.width * 0.50,
-                        child: TextInput(
-                          'Estado',
-                          'Insira seu estado',
-                          true,
-                          (String value) {
-                            return value.contains('@')
-                                ? 'Do not use the @ char.'
-                                : null;
-                          },
-                          TextInputType.text,
-                          1,
-                          focus: true,
-                          controller: _state,
-                          enabled: false,
+                      DisabledOutlineInput(
+                        labelText: 'Rua',
+                        textController: _street,
+                        lines: 1,
+                      ),
+                      DisabledOutlineInput(
+                        labelText: 'Bairro',
+                        textController: _neighborhood,
+                        lines: 1,
+                      ),
+                      DisabledOutlineInput(
+                        labelText: 'Cidade',
+                        textController: _city,
+                        lines: 1,
+                      ),
+                      Container(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            SizedBox(
+                              width: MediaQuery.of(context).size.width * 0.40,
+                              child: DisabledOutlineInput(
+                                labelText: 'Estado',
+                                textController: _state,
+                                lines: 1,
+                              ),
+                            ),
+                            SizedBox(
+                              width: MediaQuery.of(context).size.width * 0.43,
+                              child: TextInput(
+                                'Número',
+                                'Número',
+                                true,
+                                (String value) {
+                                  return value.contains('@')
+                                      ? 'Do not use the @ char.'
+                                      : null;
+                                },
+                                TextInputType.text,
+                                1,
+                                focus: true,
+                                controller: _number,
+                                enabled: true,
+                              ),
+                            )
+                          ],
                         ),
                       ),
-                      SizedBox(
-                        width: 15.0,
-                        height: 50.0,
+                      TextInput(
+                        'Complemento',
+                        'Adicione um complemento',
+                        true,
+                        (String value) {
+                          return value.contains('@')
+                              ? 'Do not use the @ char.'
+                              : null;
+                        },
+                        TextInputType.text,
+                        1,
+                        focus: true,
+                        controller: _complement,
+                        enabled: true,
+                      ),
+                      TextInput(
+                        'Ponto de referência',
+                        'Referência para sua residência',
+                        true,
+                        (String value) {
+                          return value.contains('@')
+                              ? 'Do not use the @ char.'
+                              : null;
+                        },
+                        TextInputType.text,
+                        1,
+                        focus: true,
+                        controller: _referencePoint,
+                        enabled: true,
                       ),
                       SizedBox(
-                        width: MediaQuery.of(context).size.width * 0.30,
-                        child: TextInput(
-                          'Número',
-                          'Insira o número da residência',
-                          true,
-                          (String value) {
-                            return value.contains('@')
-                                ? 'Do not use the @ char.'
-                                : null;
-                          },
-                          TextInputType.text,
-                          1,
-                          focus: true,
-                          controller: _number,
-                          enabled: true,
-                        ),
-                      )
+                        height: 10.0,
+                      ),
+                      Button(
+                        buttonText: 'Vamos lá',
+                        type: 'default',
+                        tapHandler: () async {},
+                        isSmall: false,
+                      ),
+                      SizedBox(
+                        height: 10.0,
+                      ),
                     ],
                   ),
-                ),
-                TextInput(
-                  'Complemento',
-                  'Adicione um complemento',
-                  true,
-                  (String value) {
-                    return value.contains('@')
-                        ? 'Do not use the @ char.'
-                        : null;
-                  },
-                  TextInputType.text,
-                  1,
-                  focus: true,
-                  controller: _complement,
-                  enabled: true,
-                ),
-                TextInput(
-                  'Ponto de referência',
-                  'Ajude a te encontrar com mais facilidade',
-                  true,
-                  (String value) {
-                    return value.contains('@')
-                        ? 'Do not use the @ char.'
-                        : null;
-                  },
-                  TextInputType.text,
-                  1,
-                  focus: true,
-                  controller: _referencePoint,
-                  enabled: true,
-                ),
-                SizedBox(
-                  height: 10.0,
-                ),
-                Button(
-                  buttonText: 'Vamos lá',
-                  type: 'default',
-                  tapHandler: () async {},
-                  isSmall: false,
-                ),
-                SizedBox(
-                  height: 10.0,
-                ),
-              ],
-            );
-          } else if (snapshot.hasError) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          // By default, show a loading spinner.
-          return Center(
-            child: CircularProgressIndicator(),
-          );
-        },
+                );
+              } else if (snapshot.hasError) {
+                return Center(
+                  child: TryAgainError(),
+                );
+              }
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            },
+          ),
+        ),
       ),
     );
   }
@@ -197,7 +153,6 @@ class _AdressScreenState extends State<AdressScreen> {
 
     await CEP.searchCEP('$cep', 'json', '');
 
-    print(CEP.getBody());
     var automated_address = {
       'cep': CEP.getCEP(),
       'street': CEP.getLogradouro(),
@@ -210,7 +165,6 @@ class _AdressScreenState extends State<AdressScreen> {
   }
 
   void _updateTextInputs(var data) {
-    print(data);
     _city.text = data['city'];
     _street.text = data['street'];
     _neighborhood.text = data['neighborhood'];
