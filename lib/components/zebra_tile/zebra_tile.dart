0f7d1911f2573ee5ba124b76b7f2f159bd002755
youssef-md <emaildeyoussefmuhamad@gmail.com>
2019-11-15 02:18:18 -0300

    pax-app/Wiki#222 Finish Zebra Tile and add some static income

--- lib/components/zebra_tile/zebra_tile.dart
+++ lib/components/zebra_tile/zebra_tile.dart
@@ -18,21 +18,45 @@ class ZebraTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 30),
-      color: Color.fromRGBO(0, 0, 0, .1),
+      padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 24),
+      decoration: BoxDecoration(
+        color: darken ? Color.fromRGBO(0, 0, 0, .07) : null,
+        borderRadius: BorderRadius.circular(5),
+      ),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
           Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              Text(paxName),
-              Text(username),
+              Text(
+                paxName,
+                style: Theme.of(context).textTheme.subtitle.copyWith(
+                      fontWeight: FontWeight.bold,
+                    ),
+              ),
+              SizedBox(height: 6),
+              Text(
+                date,
+                style: TextStyle(color: Colors.grey),
+              ),
             ],
           ),
           Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
             children: <Widget>[
-              Text('R\$ $money'),
-              Text(date),
+              Text(
+                '+R\$ $money',
+                style: Theme.of(context).textTheme.subtitle.copyWith(
+                    fontSize: 18,
+                    color: Theme.of(context).accentColor,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 6),
+              Text(
+                username,
+                style: TextStyle(color: Colors.grey),
+              )
             ],
           )
         ],
