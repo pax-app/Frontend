67c3cbc53f4648216531fe398bec2c48ef296cde
youssef-md <emaildeyoussefmuhamad@gmail.com>
2019-11-18 02:01:06 -0300

    pax-app/Wiki#200 Remove request in provider profile and get its service rating
    Co-authored-by: Rogério Júnior <junior.rogerio8@gmail.com>

--- lib/screens/provider_list_screen/provider_list_screen.dart
+++ lib/screens/provider_list_screen/provider_list_screen.dart
@@ -18,56 +18,73 @@ class ProviderListScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: fetchPost(),
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          var providers = snapshot.data;
-          return BaseScreen(
-            "Escolha um profissional",
-            "Prestadores de ${title}",
-            Container(
-              height: MediaQuery.of(context).size.height * .77,
-              child: ListView.builder(
-                itemCount: providers.length,
-                itemBuilder: (context, index) {
-                  return ProviderCard(
-                    providerId: providers[index]['provider_id'],
-                    name: providers[index]['name'],
-                    rating: double.parse(
-                      providers[index]['reviews_average'].toString(),
-                    ),
-                    description: providers[index]['bio'],
-                    minPrice: providers[index]['minimum_price'],
-                    maxPrice: providers[index]['maximum_price'],
-                    avatarUrl: providers[index]['url_avatar'],
-                    onTap: () => loadProvider(
-                      providers[index]['provider_id'],
-                      context,
-                    ),
-                  );
-                },
+    return Hero(
+      tag: 'provider_profile',
+      child: FutureBuilder(
+        future: _fetchPost(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            var providers = snapshot.data;
+            return BaseScreen(
+              "Escolha um profissional",
+              "Prestadores de ${title}",
+              Container(
+                height: MediaQuery.of(context).size.height * .77,
+                child: ListView.builder(
+                  itemCount: providers.length,
+                  itemBuilder: (context, index) {
+                    return ProviderCard(
+                      providerId: providers[index]['provider_id'],
+                      name: providers[index]['name'],
+                      rating: double.parse(
+                        providers[index]['reviews_average'].toString(),
+                      ),
+                      description: providers[index]['bio'],
+                      minPrice: providers[index]['minimum_price'],
+                      maxPrice: providers[index]['maximum_price'],
+                      avatarUrl: providers[index]['url_avatar'],
+                      onTap: () => loadProvider(
+                        providers[index],
+                        context,
+                      ),
+                    );
+                  },
+                ),
               ),
-            ),
-            null,
-          );
-        } else if (snapshot.hasError) {
-          return BaseScreen("", "Erro:", Text("${snapshot.error}"), null);
-        }
+              null,
+            );
+          } else if (snapshot.hasError) {
+            return BaseScreen("", "Erro:", Text("${snapshot.error}"), null);
+          }
 
-        return BaseScreen("", "Carregando", CircularProgressIndicator(), null);
-      },
+          return BaseScreen(
+              "", "Carregando", CircularProgressIndicator(), null);
+        },
+      ),
     );
   }
 
-  loadProvider(int id, ctx) {
+  loadProvider(var provider, ctx) async {
+    var reviewService =
+        await _getServiceRate(provider['provider_id'].toString());
+
     Navigator.push(
       ctx,
-      CupertinoPageRoute(builder: (ctx) => ProviderProfileScreen()),
+      CupertinoPageRoute(
+        builder: (ctx) => ProviderProfileScreen(
+          bio: provider['bio'],
+          charismaRate: double.parse(provider['reviews_average'].toString()),
+          maxPrice: provider['maximum_price'],
+          minPrice: provider['minimum_price'],
+          name: provider['name'],
+          photoUrl: provider['url_avatar'],
+          reviewService: double.parse(reviewService.toString()),
+        ),
+      ),
     );
   }
 
-  Future<dynamic> fetchPost() async {
+  Future<dynamic> _fetchPost() async {
     final response = await http.get(
         'https://pax-user.herokuapp.com/provider_by_category/review/$providerCategoryId');
     var responseJson = json.decode(response.body);
@@ -79,5 +96,13 @@ class ProviderListScreen extends StatelessWidget {
     }
 
     return listaFinal;
+  }
+
+  Future<dynamic> _getServiceRate(String providerId) async {
+    final response = await http.get(
+        'https://pax-review.herokuapp.com/service_reviews/average/$providerId');
+    var responseJson = json.decode(response.body);
+
+    return responseJson['provider_service_review_average'];
   }
 }
--- lib/screens/provider_profile_screen/provider_profile_screen.dart
+++ lib/screens/provider_profile_screen/provider_profile_screen.dart
@@ -2,175 +2,162 @@ import 'package:Pax/components/app_bar/white_appbar.dart';
 import 'package:Pax/theme/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:smooth_star_rating/smooth_star_rating.dart';
-import 'dart:async';
-import 'dart:io';
-import 'dart:convert';
-import 'package:http/http.dart' as http;
-import 'package:Pax/components/base_screen/base_screen.dart';
-import 'package:Pax/models/Provider.dart';
-
-Future<Provider> fetchPost() async {
-  final response = await http.get(
-      'http://pax-user.herokuapp.com/provider_info_by_category?providerId=&categoryId=',
-      //If here we receive category name instead of id then just change the name of the query parameter to 'name'
-      headers: {HttpHeaders.contentTypeHeader: 'application/json'});
-  var responseJson = json.decode(response.body);
-
-  final Provider infoProvider = Provider.fromJson(responseJson);
-
-  return infoProvider;
-}
 
 class ProviderProfileScreen extends StatelessWidget {
-  ProviderProfileScreen({Key key}) : super(key: key);
+  final String photoUrl;
+  final String name;
+  final double reviewService;
+  final double charismaRate;
+  final String bio;
+  final double minPrice;
+  final double maxPrice;
+
+  ProviderProfileScreen({
+    @required this.photoUrl,
+    @required this.name,
+    @required this.reviewService,
+    @required this.bio,
+    @required this.minPrice,
+    @required this.maxPrice,
+    @required this.charismaRate,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<Provider>(
-      future: fetchPost(),
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          Provider providerInfo = snapshot.data;
-          return Scaffold(
-            appBar: WhiteAppBar(
-              "",
-              context,
-              actions: <Widget>[
-                FlatButton(
-                  onPressed: () {},
-                  textColor: secondaryColor,
-                  child: Text(
-                    "CHAT",
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  shape: CircleBorder(
-                    side: BorderSide(color: Colors.transparent),
-                  ),
-                )
-              ],
+    return Scaffold(
+      appBar: WhiteAppBar(
+        "",
+        context,
+        actions: <Widget>[
+          FlatButton(
+            onPressed: () {},
+            textColor: secondaryColor,
+            child: Text(
+              "CHAT",
+              style: TextStyle(fontWeight: FontWeight.bold),
             ),
-            body: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Card(
-                  margin: EdgeInsets.all(0),
-                  shape:
-                      RoundedRectangleBorder(borderRadius: BorderRadius.zero),
-                  child: Container(
-                    padding: EdgeInsets.only(
-                        top: 30, left: 30, right: 30, bottom: 20),
-                    child: Column(
+            shape: CircleBorder(
+              side: BorderSide(color: Colors.transparent),
+            ),
+          )
+        ],
+      ),
+      body: Hero(
+        tag: 'provider_profile',
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Card(
+              margin: EdgeInsets.all(0),
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.zero),
+              child: Container(
+                padding:
+                    EdgeInsets.only(top: 30, left: 30, right: 30, bottom: 20),
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
-                        Row(
+                        Column(
+                          children: <Widget>[
+                            Container(
+                              width: 120,
+                              height: 120,
+                              margin: EdgeInsets.only(right: 30),
+                              decoration: new BoxDecoration(
+                                borderRadius: BorderRadius.circular(25),
+                                image: DecorationImage(
+                                    image: NetworkImage(photoUrl),
+                                    fit: BoxFit.cover),
+                                border:
+                                    Border.all(color: secondaryColor, width: 3),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: <Widget>[
-                            Column(
-                              children: <Widget>[
-                                Container(
-                                  width: 120,
-                                  height: 120,
-                                  margin: EdgeInsets.only(right: 30),
-                                  decoration: new BoxDecoration(
-                                    borderRadius: BorderRadius.circular(25),
-                                    image: DecorationImage(
-                                        image: NetworkImage(
-                                            providerInfo.providerPhoto),
-                                        fit: BoxFit.cover),
-                                    border: Border.all(
-                                        color: secondaryColor, width: 3),
-                                  ),
-                                ),
-                              ],
+                            Text(
+                              name,
+                              style: Theme.of(context).textTheme.title,
+                            ),
+                            SizedBox(
+                              height: 10,
                             ),
                             Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: <Widget>[
                                 Text(
-                                  providerInfo.providerName,
-                                  style: Theme.of(context).textTheme.title,
+                                  "Serviço",
                                 ),
-                                SizedBox(
-                                  height: 10,
+                                SmoothStarRating(
+                                    allowHalfRating: false,
+                                    rating: reviewService,
+                                    size: 20,
+                                    color: secondaryColor,
+                                    borderColor: secondaryColor),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  "Carisma",
                                 ),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: <Widget>[
-                                    Text(
-                                      "Serviço",
-                                    ),
-                                    SmoothStarRating(
-                                        allowHalfRating: false,
-                                        rating: providerInfo.reviewService,
-                                        size: 20,
-                                        color: secondaryColor,
-                                        borderColor: secondaryColor),
-                                  ],
-                                ),
-                                SizedBox(
-                                  height: 10,
-                                ),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: <Widget>[
-                                    Text(
-                                      "Carisma",
-                                    ),
-                                    SmoothStarRating(
-                                        allowHalfRating: false,
-                                        rating: providerInfo.charismaRate,
-                                        size: 20,
-                                        color: secondaryColor,
-                                        borderColor: secondaryColor),
-                                  ],
+                                SmoothStarRating(
+                                  allowHalfRating: false,
+                                  rating: charismaRate,
+                                  size: 20,
+                                  color: secondaryColor,
+                                  borderColor: secondaryColor,
                                 ),
                               ],
-                            )
-                          ],
-                        ),
-                        Row(
-                          children: <Widget>[
-                            Flexible(
-                              child: Container(
-                                margin: EdgeInsets.only(top: 20),
-                                child: Text(
-                                  providerInfo.bio,
-                                  style: TextStyle(fontSize: 15),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                        Row(
-                          children: <Widget>[
-                            Container(
-                              margin: EdgeInsets.only(top: 15),
-                              child: Text(
-                                "R\$" +
-                                    providerInfo.minPrice.toString() +
-                                    " a " +
-                                    "R\$" +
-                                    providerInfo.maxPrice.toString(),
-                                style: TextStyle(
-                                    color: secondaryColor, fontSize: 16),
-                              ),
                             ),
                           ],
                         )
                       ],
                     ),
-                  ),
+                    Row(
+                      children: <Widget>[
+                        Flexible(
+                          child: Container(
+                            margin: EdgeInsets.only(top: 20),
+                            child: Text(
+                              bio,
+                              style: TextStyle(fontSize: 15),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Container(
+                          margin: EdgeInsets.only(top: 15),
+                          child: Text(
+                            "R\$" +
+                                minPrice.toString() +
+                                " a " +
+                                "R\$" +
+                                maxPrice.toString(),
+                            style:
+                                TextStyle(color: secondaryColor, fontSize: 16),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
                 ),
-              ],
+              ),
             ),
-          );
-        } else if (snapshot.hasError) {
-          return BaseScreen("", "Erro:", Text("${snapshot.error}"), null);
-        }
-
-        // By default, show a loading spinner.
-        return BaseScreen("", "Carregando", CircularProgressIndicator(), null);
-      },
+          ],
+        ),
+      ),
     );
   }
 }
